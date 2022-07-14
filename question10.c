2fe518f0b1272586484b85c050cea4e8975b2658
ravishankar8541 <107765864+ravishankar8541@users.noreply.github.com>
2022-07-14 21:22:54 +0530

    Update question10.c

--- question10.c
+++ question10.c
@@ -6,9 +6,9 @@ Output format –
 #include"stdio.h"
 int main()
 {
-    int DD,MM,YYYY;
+    int day,month,year;
     printf("DD/MM/YYYY\n");
-    scanf("%d%d%d",&DD,&MM,&YYYY);
-    printf("\"Day - %d , Month - %d , Year - %d\" ",DD,MM,YYYY);
+    scanf("%d/%d/%d",&day,&month,&year);
+    printf("\"Day - %d , Month - %d , Year - %d\" ",day,month,year);
     return 0;
 }
